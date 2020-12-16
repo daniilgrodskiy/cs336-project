bf7410e4922fcc4a42cff4b8f61706ecec0c0aa6
daniilgrodskiy <35942703+daniilgrodskiy@users.noreply.github.com>
2020-12-15 21:18:46 -0500

    Add 'daniil' package to get access to some classes

---
+++ src/daniil/Train.java
@@ -0,0 +1,17 @@
+package daniil;
+
+public class Train {
+    private int tid;
+
+    public Train(int tid) {
+        this.tid = tid;
+    }
+
+    public int getTid() {
+        return tid;
+    }
+
+    public void setTid(int tid) {
+        this.tid = tid;
+    }
+}
